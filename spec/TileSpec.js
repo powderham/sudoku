dbcd17a95a52171b69b6ae1b1b86a37d891e1272
powderham <edpowderham@outlook.com>
2016-12-30 19:50:22 +0000

    refactoring rows

--- spec/TileSpec.js
+++ spec/TileSpec.js
@@ -6,15 +6,24 @@ describe("Tile", function() {
     });
 
   it("should contain 3 individual rows", function(){
+    expect(tile.row0()).toBeTruthy()
     expect(tile.row1()).toBeTruthy()
     expect(tile.row2()).toBeTruthy()
-    expect(tile.row3()).toBeTruthy()
   })
 
   it("should contain 3 individual rows with three data points", function(){
+    expect(tile.row0()).toEqual([null,null,null])
     expect(tile.row1()).toEqual([null,null,null])
     expect(tile.row2()).toEqual([null,null,null])
-    expect(tile.row3()).toEqual([null,null,null])
 })
+
+  it("should allow numbers to be input in to the tile", function(){
+    tile.addNumber(0,0,1)
+    expect(tile.row1()[0]).toEqual(1)
+  })
+
+  it("should be able to return existing values", function(){
+    expect(tile.existingNumbers()).toEqual([])
+  })
 
 });
--- src/Tile.js
+++ src/Tile.js
@@ -1,9 +1,13 @@
 function Tile() {
+  this._row0 = [null,null,null]
   this._row1 = [null,null,null]
   this._row2 = [null,null,null]
-  this._row3 = [null,null,null]
 }
 
+
+Tile.prototype.row0 = function() {
+  return this._row0
+};
 
 Tile.prototype.row1 = function() {
   return this._row1
@@ -13,6 +17,6 @@ Tile.prototype.row2 = function() {
   return this._row2
 };
 
-Tile.prototype.row3 = function() {
-  return this._row3
+Tile.prototype.addNumber = function (x,y,number) {
+  console.log(eval("this._row"+x))
 };
