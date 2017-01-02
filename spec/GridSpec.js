161107f54fb132b5bc77472631f0bcb63ca64537
powderham <edpowderham@outlook.com>
2017-01-02 17:19:24 +0000

    Adding number to tile via grid

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -3,6 +3,10 @@ describe("Grid", function() {
 
   beforeEach(function() {
       grid = new Grid();
-    });
+  });
 
+  it("can tell a tile to add a number to itself", function(){
+    grid.addNumberToTile(0,0,0,0,1)
+    expect(grid.retrieveTile(0,0)._index[0][0]).toEqual(1)
+  })
 });
--- src/Grid.js
+++ src/Grid.js
@@ -1,7 +1,16 @@
 function Grid() {
   this._index = [
-      [[new Tile()],[new Tile()],[new Tile()]],
-      [[new Tile()],[new Tile()],[new Tile()]],
-      [[new Tile()],[new Tile()],[new Tile()]]
+      [new Tile(),new Tile(),new Tile()],
+      [new Tile(),new Tile(),new Tile()],
+      [new Tile(),new Tile(),new Tile()]
     ]
-}
+};
+
+Grid.prototype.addNumberToTile = function (xGrid,yGrid, xTile, yTile, number) {
+  var tile = this.retrieveTile(xGrid, yGrid)
+  tile.addNumber(xTile, yTile, number)
+};
+
+Grid.prototype.retrieveTile = function (x,y) {
+  return this._index[x][y]
+};
