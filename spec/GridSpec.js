13b64fb9d1803a98108f622c662beb558db89d75
powderham <edpowderham@outlook.com>
2017-01-03 21:14:31 +0000

    adding functions to return a specific row/column from tile

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -9,4 +9,30 @@ describe("Grid", function() {
     grid.addNumberToTile(0,0,0,0,1)
     expect(grid.retrieveTile(0,0)._index[0][0]).toEqual(1)
   })
+
+  it("can return all numbers in the same column", function(){
+    grid.addNumberToTile(0,0,0,0,1)
+    grid.addNumberToTile(0,0,1,0,2)
+    grid.addNumberToTile(0,0,2,0,3)
+    grid.addNumberToTile(1,0,0,0,4)
+    grid.addNumberToTile(1,0,1,0,5)
+    grid.addNumberToTile(1,0,2,0,6)
+    grid.addNumberToTile(2,0,0,0,7)
+    grid.addNumberToTile(2,0,1,0,8)
+    grid.addNumberToTile(2,0,2,0,9)
+    expect(grid.returnColumnNumbers(0,0,0,0)).toEqual([1,2,3,4,5,6,7,8,9])
+  })
+
+  it("can return all numbers in the same row", function(){
+    grid.addNumberToTile(0,0,0,0,1)
+    grid.addNumberToTile(0,0,0,1,2)
+    grid.addNumberToTile(0,0,0,2,3)
+    grid.addNumberToTile(0,1,0,0,4)
+    grid.addNumberToTile(0,1,0,1,5)
+    grid.addNumberToTile(0,1,0,2,6)
+    grid.addNumberToTile(0,2,0,0,7)
+    grid.addNumberToTile(0,2,0,1,8)
+    grid.addNumberToTile(0,2,0,2,9)
+    expect(grid.returnRowNumbers(0,0,0,0)).toEqual([1,2,3,4,5,6,7,8,9])
+  })
 });
--- src/Grid.js
+++ src/Grid.js
@@ -14,3 +14,26 @@ Grid.prototype.addNumberToTile = function (xGrid,yGrid, xTile, yTile, number) {
 Grid.prototype.retrieveTile = function (x,y) {
   return this._index[x][y]
 };
+
+Grid.prototype.returnColumnNumbers = function (xGrid, yGrid, xTile, yTile) {
+  var array = []
+  for (var gridRow in this._index ){
+    var tile = this._index[gridRow][xGrid]
+    for (var tileRow in tile._index){
+      array.push(tile._index[tileRow][yGrid])
+    }
+  }
+  return array
+};
+
+Grid.prototype.returnRowNumbers = function (xGrid, yGrid, xTile, yTile) {
+  var array = []
+  var row = this._index[yGrid]
+  for (var gridRow in row){
+    var tile = this._index[yGrid][gridRow]
+    for (var index in tile._index[xTile]){
+      array.push(tile._index[xTile][index])
+    }
+  }
+  return array
+};
